7b34caa410579fdcd18ba07ad773e4482b47b493
GKX051106 <160452160+GKX051106@users.noreply.github.com>
2024-02-20 15:43:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ def index():
 @app.route("/main",method=["GET","POST"])
 def main():
   r = request.form.get("r")
-    return(render_template("main.html",r=r))
+  return(render_template("main.html",r=r))
 
 @app.route("/image_gpt",method=["GET","POST"])
 def image_gpt():
   r = request.form.get("r")
-    return(render_template("image_gpt.html",r=r))
+  return(render_template("image_gpt.html",r=r))
 
 
 if __name__=="__main__":
