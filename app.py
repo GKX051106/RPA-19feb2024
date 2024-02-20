2683317b7fe06060e056f8d4a22f7fcee388a10a
GKX051106 <160452160+GKX051106@users.noreply.github.com>
2024-02-20 16:37:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ first_time = 1
 def index():
   return(render_template("index.html"))
 
-@app.route("/main",method=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
   global r,first_time
   if first_time==1:
     r = request.form.get("r")
     return(render_template("main.html",r=r))
 
-@app.route("/image_gpt",method=["GET","POST"])
+@app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
   r = request.form.get("r")
   return(render_template("image_gpt.html",r=r))
