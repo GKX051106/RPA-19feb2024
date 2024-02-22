d9aaeb1d8878030dd7f290509491a444f87a3a86
GKX051106 <160452160+GKX051106@users.noreply.github.com>
2024-02-22 17:30:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,6 @@ def image_result():
 
 @app.route("/re_image_result",methods=["GET","POST"])
 def re_image_result():
-    q = request.form.get("q")
     r = replicate.run(
     "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
     input={
